934c9a3e492f0ab989f26ccc18c4e64bbe315eb3
Gustavo <gusstavocta@gmail.com>
2025-02-25 23:32:32 -0500

    implementacion de navigation

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:widgets_app/config/menu/menu_items.dart';
+import 'package:widgets_app/presentation/screens/buttons/buttons_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -46,7 +47,10 @@ class _CustomListile extends StatelessWidget {
         trailing: Icon(Icons.chevron_right, color: colors.primary),
         leading: Icon(menuItem.icon, color: colors.primary),
         onTap: () => {
-              //TODO: Implement navigation
+              // Navigator.of(context).push(MaterialPageRoute(
+              //     builder: (context) => const ButtonsScreen()))
+
+              Navigator.of(context).pushNamed(menuItem.link)
             });
   }
 }
