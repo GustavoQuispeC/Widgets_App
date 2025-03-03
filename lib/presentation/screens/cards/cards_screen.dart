980643bd4bf26214198da957770b36dee42a856b
Gustavo <gusstavocta@gmail.com>
2025-03-02 23:52:41 -0500

    implementando imagenes en las tarjetas

--- lib/presentation/screens/cards/cards_screen.dart
+++ lib/presentation/screens/cards/cards_screen.dart
@@ -33,8 +33,18 @@ class _CardsView extends StatelessWidget {
       child: Column(children: [
         ...cards.map((card) =>
             _CardType1(elevation: card['elevation'], label: card['label'])),
+
         ...cards.map((card) =>
-            _CardType1(elevation: card['elevation'], label: card['label']))
+            _CardType2(elevation: card['elevation'], label: card['label'])),
+
+        ...cards.map((card) =>
+            _CardType3(elevation: card['elevation'], label: card['label'])),
+
+        ...cards.map((card) =>
+            _CardType4(elevation: card['elevation'], label: card['label'])),
+
+        //agrega un espacio al final
+        const SizedBox(height: 50),
       ]),
     );
   }
@@ -67,6 +77,123 @@ class _CardType1 extends StatelessWidget {
             ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class _CardType2 extends StatelessWidget {
+  final String label;
+  final double elevation;
+
+  const _CardType2({required this.label, required this.elevation});
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: const BorderRadius.all(Radius.circular(12)),
+        side: BorderSide(
+          color: colors.outline,
+        ),
+      ),
+      elevation: elevation,
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(10, 5, 10, 10),
+        child: Column(
+          children: [
+            Align(
+              alignment: Alignment.topRight,
+              child: IconButton(
+                icon: const Icon(Icons.more_vert_outlined),
+                onPressed: () {},
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomLeft,
+              child: Text('$label - outline'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _CardType3 extends StatelessWidget {
+  final String label;
+  final double elevation;
+
+  const _CardType3({required this.label, required this.elevation});
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
+    return Card(
+      color: colors.surfaceBright,
+      elevation: elevation,
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(10, 5, 10, 10),
+        child: Column(
+          children: [
+            Align(
+              alignment: Alignment.topRight,
+              child: IconButton(
+                icon: const Icon(Icons.more_vert_outlined),
+                onPressed: () {},
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomLeft,
+              child: Text('$label - filled'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _CardType4 extends StatelessWidget {
+  final String label;
+  final double elevation;
+
+  const _CardType4({required this.label, required this.elevation});
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
+    return Card(
+      clipBehavior: Clip.hardEdge,
+      color: colors.surfaceBright,
+      elevation: elevation,
+      child: Stack(
+        children: [
+          Image.network(
+            'https://picsum.photos/id/${elevation.toInt()}/600/350',
+            height: 350,
+            fit: BoxFit.cover,
+          ),
+          Align(
+            alignment: Alignment.topRight,
+            child: Container(
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(20),
+                ),
+              ),
+              child: IconButton(
+                icon: const Icon(Icons.more_vert_outlined),
+                onPressed: () {},
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
