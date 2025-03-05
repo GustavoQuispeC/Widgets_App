223658e1ce9e60f5d0ecdf7f9d2dc87f49e8dc71
Gustavo <gusstavocta@gmail.com>
2025-03-05 00:01:21 -0500

    implementando Dialog

--- lib/presentation/screens/snackbar/snackbar_screen.dart
+++ lib/presentation/screens/snackbar/snackbar_screen.dart
@@ -5,6 +5,7 @@ class SnackbarScreen extends StatelessWidget {
 
   const SnackbarScreen({super.key});
 
+//sirve para mostrar un snackbar
   void showCustomSnackbar(BuildContext context) {
     ScaffoldMessenger.of(context).clearSnackBars();
 
@@ -19,12 +20,57 @@ class SnackbarScreen extends StatelessWidget {
     ScaffoldMessenger.of(context).showSnackBar(snackback);
   }
 
+//sirve para mostrar un dialogo
+  void openDialog(BuildContext context) {
+    showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: (context) {
+          return AlertDialog(
+            title: const Text('Diálogo de ejemplo'),
+            content: const Text('Este es un diálogo de ejemplo'),
+            actions: [
+              TextButton(
+                onPressed: () => Navigator.of(context).pop(),
+                child: const Text('Cerrar'),
+              )
+            ],
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Snackbars y Dialogs'),
       ),
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          //? creando un dialogo de licencias
+          FilledButton.tonal(
+              onPressed: () {
+                showAboutDialog(context: context, children: [
+                  const Text('Aplicación de ejemplo'),
+                  const Text('Versión 1.0.0'),
+                  const Text('Desarrollado por:'),
+                  const Text('Gustavo Quispe Castañeda')
+                ]);
+              },
+              child: const Text('Licencias usadas')),
+
+          //? creando un dialogo de ejemplo
+          FilledButton.tonal(
+              onPressed: () {
+                openDialog(context);
+              },
+              child: const Text('Mostrar diálogo')),
+        ],
+      )),
+
+      //?creando un snackbar
       floatingActionButton: FloatingActionButton.extended(
         label: const Text('Show Snackbar'),
         icon: const Icon(Icons.remove_red_eye_outlined),
