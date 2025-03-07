882aa216bd6aacb882104dfc58429973e7981dad
Gustavo <gusstavocta@gmail.com>
2025-03-06 22:45:55 -0500

    animation terminado

--- lib/presentation/screens/animated/animated_screen.dart
+++ lib/presentation/screens/animated/animated_screen.dart
@@ -1,10 +1,41 @@
 import 'package:flutter/material.dart';
+import 'dart:math' show Random;
 
-class AnimatedScreen extends StatelessWidget {
+class AnimatedScreen extends StatefulWidget {
   static const String name = 'animated_screen';
 
   const AnimatedScreen({super.key});
 
+  @override
+  State<AnimatedScreen> createState() => _AnimatedScreenState();
+}
+
+//? creando un estado para el widget
+class _AnimatedScreenState extends State<AnimatedScreen> {
+  double width = 50;
+  double height = 50;
+  Color color = Colors.indigo;
+  double borderRadius = 10;
+
+  void changeShape() {
+    final random = Random();
+
+    width = random.nextInt(300) + 50.toDouble();
+    height = random.nextInt(300) + 50.toDouble();
+    borderRadius = random.nextInt(100).toDouble() + 20;
+    color = Color.fromRGBO(
+        //red
+        random.nextInt(256),
+        //green
+        random.nextInt(256),
+        //blue
+        random.nextInt(256),
+        //opacity
+        1);
+    setState(() {});
+  }
+
+//? creando un contenedor animado
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,19 +45,20 @@ class AnimatedScreen extends StatelessWidget {
       body: Center(
         child: AnimatedContainer(
           duration: const Duration(seconds: 1),
-          curve: Curves.easeOutCubic,
-          width: 300,
-          height: 100,
+          curve: Curves.fastOutSlowIn,
+          width: width <= 0 ? 0 : width,
+          height: height <= 0 ? 0 : height,
           decoration: BoxDecoration(
-            color: Colors.blue,
-            borderRadius: BorderRadius.circular(10),
+            color: color,
+            borderRadius:
+                BorderRadius.circular(borderRadius < 0 ? 0 : borderRadius),
           ),
         ),
       ),
 
       //? creando un botón flotante
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: changeShape,
         child: const Icon(Icons.play_arrow),
       ),
     );
