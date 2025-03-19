e98abd217f2e7dcdb318ff0c938d0489f63b63eb
Gustavo <gusstavocta@gmail.com>
2025-03-19 00:00:55 -0500

    tutorial finalizado

--- lib/presentation/screens/app_tutorial/app_tutorial_screen.dart
+++ lib/presentation/screens/app_tutorial/app_tutorial_screen.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
@@ -24,35 +25,76 @@ final slides = <SlideInfo>[
       'assets/images/3.png'),
 ];
 
-class AppTutorialScreen extends StatelessWidget {
+class AppTutorialScreen extends StatefulWidget {
   static const String name = 'app_tutorial_screen';
 
   const AppTutorialScreen({super.key});
 
   @override
+  State<AppTutorialScreen> createState() => _AppTutorialScreenState();
+}
+
+//!controlador de la pagina
+class _AppTutorialScreenState extends State<AppTutorialScreen> {
+  late final PageController pageviewController = PageController();
+  bool endReached = false;
+  @override
+  void initState() {
+    super.initState();
+    pageviewController.addListener(() {
+      final page = pageviewController.page ?? 0;
+      if (!endReached && page >= slides.length - 1.5) {
+        setState(() {
+          endReached = true;
+        });
+      }
+    });
+  }
+
+//eliminar el controlador de la pagina cuando se cierre la pantalla
+  @override
+  void dispose() {
+    pageviewController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: Stack(children: [
-        PageView(
-          physics: const BouncingScrollPhysics(),
-          children: slides
-              .map((slideData) => _Slide(
-                    title: slideData.title,
-                    caption: slideData.caption,
-                    imageUrl: slideData.imageUrl,
-                  ))
-              .toList(),
-        ),
-        Positioned(
-            right: 20,
-            top: 50,
-            child: TextButton(
-              child: const Text('Salir'),
-              onPressed: () => context.pop(),
-            ))
-      ]),
-    );
+        backgroundColor: Colors.white,
+        body: Stack(children: [
+          PageView(
+            controller: pageviewController,
+            physics: const BouncingScrollPhysics(),
+            children: slides
+                .map((slideData) => _Slide(
+                      title: slideData.title,
+                      caption: slideData.caption,
+                      imageUrl: slideData.imageUrl,
+                    ))
+                .toList(),
+          ),
+          Positioned(
+              right: 20,
+              top: 50,
+              child: TextButton(
+                child: const Text('Salir'),
+                onPressed: () => context.pop(),
+              )),
+          endReached
+              ? Positioned(
+                  bottom: 30,
+                  right: 30,
+                  child: FadeInRight(
+                    from: 15,
+                    delay: const Duration(seconds: 1),
+                    child: FilledButton(
+                        onPressed: () => context.pop(),
+                        child: const Text('Comenzar')),
+                  ),
+                )
+              : const SizedBox()
+        ]));
   }
 }
 
