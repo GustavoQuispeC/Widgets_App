87afa20ecad0a2e197bd1f45942950b06c596099
Gustavo <gusstavocta@gmail.com>
2025-03-20 00:10:55 -0500

    finalizando scroll

--- lib/presentation/screens/infinite_scroll/infinite_scroll_screen.dart
+++ lib/presentation/screens/infinite_scroll/infinite_scroll_screen.dart
@@ -11,7 +11,55 @@ class InfiniteScrollScreen extends StatefulWidget {
 }
 
 class _InfiniteScrollScreenState extends State<InfiniteScrollScreen> {
+  //Lista de imagenes
   List<int> imagesIds = [1, 2, 3, 4, 5];
+  //Controlador de scroll
+  final ScrollController scrollController = ScrollController();
+  //Bandera para saber si esta cargando
+  bool isLoading = false;
+  //Bandera para saber si esta montado el componente/widget
+  bool isMounted = true;
+
+//Se ejecuta antes de que el widget sea construido
+  @override
+  void initState() {
+    super.initState();
+    scrollController.addListener(() {
+      if (scrollController.position.pixels + 500 >=
+          scrollController.position.maxScrollExtent) {
+        loadNextPage();
+      }
+    });
+  }
+
+  //Se ejecuta antes de que el widget sea destruido
+  @override
+  void dispose() {
+    scrollController.dispose();
+    isMounted = false;
+    super.dispose();
+  }
+
+//Carga la siguiente pagina
+  Future loadNextPage() async {
+    if (isLoading) return;
+    isLoading = true;
+    await Future.delayed(const Duration(seconds: 2));
+    addFiveImages();
+    isLoading = false;
+
+    //todo: revisar si esta montado el componente/widget
+    if (!isMounted) return;
+    setState(() {});
+  }
+
+//Agrega 5 imagenes a la lista
+  void addFiveImages() {
+    final lastId = imagesIds.last;
+    imagesIds.addAll([1, 2, 3, 4, 5].map((e) => lastId + e));
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,6 +71,7 @@ class _InfiniteScrollScreenState extends State<InfiniteScrollScreen> {
         removeBottom: true, //no funciona en algunos dispositivos moviles
         child: ListView.builder(
             physics: const BouncingScrollPhysics(), //efecto de rebote
+            controller: scrollController,
             itemCount: imagesIds.length,
             itemBuilder: (context, index) {
               return FadeInImage(
