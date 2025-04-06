8dbbb4f79ca467c8a1484918a0b002ab4725e679
Gustavo <gusstavocta@gmail.com>
2025-04-05 23:21:02 -0500

    implementando el contador desde el provider

--- lib/presentation/screens/counter_screen/counter_screen.dart
+++ lib/presentation/screens/counter_screen/counter_screen.dart
@@ -1,17 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:widgets_app/presentation/providers/counter_provider.dart';
 
-class CounterScreen extends StatelessWidget {
+class CounterScreen extends ConsumerWidget {
   static const String name = 'counter_screen';
+
   const CounterScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final count = ref.watch(counterProvider); //! Lee el valor
     return Scaffold(
       appBar: AppBar(
         title: const Text('Counter Screen'),
       ),
-      body: const Center(
-        child: Text('Valor:10'),
+      body: Center(
+        child: Text('Valor $count',
+            style: Theme.of(context).textTheme.headlineLarge),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Navigator.pushNamed(context, CounterScreen.name);
+        },
+        child: const Icon(Icons.add),
       ),
     );
   }
