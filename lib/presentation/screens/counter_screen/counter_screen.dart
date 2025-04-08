953fbbc85f9a31938c9238fb90c7c8b842ec6dc2
Gustavo <gusstavocta@gmail.com>
2025-04-07 22:15:29 -0500

    implementacion del tema usando provider

--- lib/presentation/screens/counter_screen/counter_screen.dart
+++ lib/presentation/screens/counter_screen/counter_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:widgets_app/presentation/providers/counter_provider.dart';
+import 'package:widgets_app/presentation/providers/theme_provider.dart';
 
 class CounterScreen extends ConsumerWidget {
   static const String name = 'counter_screen';
@@ -10,9 +11,21 @@ class CounterScreen extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final count = ref.watch(counterProvider); //! Lee el valor
+    final bool isDarkMode = ref.watch(isDarkModeProvider); //! Lee el valor
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Counter Screen'),
+        actions: [
+          IconButton(
+            icon: isDarkMode
+                ? const Icon(Icons.light_mode_outlined)
+                : const Icon(Icons.dark_mode_outlined),
+            onPressed: () {
+              ref.read(isDarkModeProvider.notifier).update((state) => !state);
+            },
+          ),
+        ],
       ),
       body: Center(
         child: Text('Valor $count',
@@ -20,7 +33,9 @@ class CounterScreen extends ConsumerWidget {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          // Navigator.pushNamed(context, CounterScreen.name);
+          ref
+              .read(counterProvider.notifier)
+              .state++; // Navigator.pushNamed(context, CounterScreen.name);
         },
         child: const Icon(Icons.add),
       ),
